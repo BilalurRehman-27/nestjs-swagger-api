a904cf706cc8e10d0a32ca26ac59b1b7d1bd41ca
Bilal Ur Rehman <n3rdybynatur37201@gmail.com>
2024-05-31 07:05:26 +0500

    feat: Add Swagger UI customization options
    This commit adds customization options to the Swagger UI setup in the `main.ts` file. The customizations include setting a custom site title, favicon, JavaScript and CSS URLs for Swagger UI. Additionally, the commit enables global validation using class-validator and sets a global prefix for the API endpoints.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,12 +22,28 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('/', app, document, {
-    swaggerOptions: { defaultModelsExpandDepth: -1 },
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
   });
+  // SwaggerModule.setup('/', app, document, {
+  //   swaggerOptions: { defaultModelsExpandDepth: -1 },
+  // });
+
   // efaultModelsExpandDepth: -1 ==> Hide models schemas section
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api/v1');
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(5000);
 }
 bootstrap();
